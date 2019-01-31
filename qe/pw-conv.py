bf393eb92aedf229000f0a262ee181e9d56dd292
Robert Wexler <rwexler@modv-ve504-0125.apn.wlan.upenn.edu>
2019-01-31 11:56:49 -0500

    changed conv-test to pw-conv

--- qe/pw-conv.py
+++ qe/pw-conv.py
@@ -1,8 +1,9 @@
+import sys
 import os
 import numpy as np
 
-filename = "asn.in"
-values = np.arange(30, 75, 5)
+filename = sys.argv[1]
+values = np.arange(20, 75, 5)
 
 old_file = open(filename, "r")
 
@@ -18,12 +19,16 @@ for value in values :
             break
         else :
             new_file.write(line)
-    
+    for line in old_file :
+        if "ecutrho" in line :
+            new_file.write("ecutrho = " + str(value * 10) + ",\n")
+            break
+        else :
+            new_file.write(line)
     for line in old_file :
         new_file.write(line)
 
     os.system("cp ../runscript ./")
-    os.system("cp ../*.upf ./")
     os.system("qsub runscript")
 
     old_file.seek(0)
