34ba6dcfe30121e2de638f11da919735da826c4c
robertwexler <rwexler@sas.upenn.edu>
2020-07-15 09:03:17 -0400

    Reformatted energy.py

--- vasp/energy.py
+++ vasp/energy.py
@@ -1,7 +1,22 @@
-energies = list()
-with open("OUTCAR", "r") as file :
-	for line in file :
-		if "energy(sigma->0)" in line :
-			energies.append(line.split()[-1])
-energy = energies[-1]
-print(energy)
+#!/usr/bin/env python
+"""
+Program for getting the final total energy extrapolated to zero smearing
+"""
+
+__author__ = 'Robert B. Wexler'
+__email__ = 'robertowexler@gmail.com'
+
+
+def main():
+    """
+Main program
+    """
+    with open('OUTCAR') as f:
+        for line in f:
+            if 'energy(sigma->0)0' in line:
+                energy = line.split()[-1]
+    print(energy)
+
+
+if __name__ == '__main__':
+    main()
